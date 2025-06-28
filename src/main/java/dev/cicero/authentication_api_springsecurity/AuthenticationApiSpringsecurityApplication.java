0b1410f2780b7948f44e82c3984927d96729621b
Cicero Guilherme <cicerog.silvestre@gmail.com>
2025-06-28 00:19:53 -0300

    firt commit

---
+++ src/main/java/dev/cicero/authentication_api_springsecurity/AuthenticationApiSpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package dev.cicero.authentication_api_springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationApiSpringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApiSpringsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cicero/authentication_api_springsecurity/AuthenticationApiSpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cicero.authentication_api_springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApiSpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
