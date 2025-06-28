71858cdebcce557e3309457f495903d2a4bbd827
Cicero Guilherme <cicerog.silvestre@gmail.com>
2025-06-28 00:53:43 -0300

    feat: realizada criacao do token service para criptografia

---
+++ src/main/java/dev/cicero/authentication_api_springsecurity/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package dev.cicero.authentication_api_springsecurity.repositories;
+
+import dev.cicero.authentication_api_springsecurity.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
